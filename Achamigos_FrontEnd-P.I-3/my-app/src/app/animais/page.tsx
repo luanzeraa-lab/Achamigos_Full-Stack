e53d79222d6d585d423721947ffaccfe53d5233b
Luan Silva de Picoli <luansilva_2@hotmail.com>
2025-11-08 16:36:23 -0300

    Animais

--- Achamigos_FrontEnd-P.I-3/my-app/src/app/animais/page.tsx
+++ Achamigos_FrontEnd-P.I-3/my-app/src/app/animais/page.tsx
@@ -13,16 +13,25 @@ import { useEffect, useState } from 'react';
 const Animais = () => {
   const [animal, setAnimal] = useState<IAnimal[]>([]);
 
-  // COMENTADO PARA NAO DAR ERRO NA HORA DE FAZER O FRONT, SO TIRAR O COMENTARIO DEPOIS
-  // useEffect(() => {
-  //   const listaAnimal = async () => {
-  //    const response = await axios.get('http://localhost:3002/cadastroanimal');
+  
+  useEffect(() => {
+  const listaAnimal = async () => {
+    try {
+      const res = await axios.get('http://localhost:3002/animais', {
+        headers: {
+          'x-api-key': '1234', 
+        },
+      });
 
-  //     setAnimal(response.data);
-  //      console.log(response.data);
-  //   };
-  //   listaAnimal();
-  //  }, []);
+      setAnimal(res.data);
+      console.log(res.data);
+    } catch (err) {
+      console.error('Erro ao buscar animais:', err);
+    }
+  };
+
+  listaAnimal();
+}, []);
 
 
   return (
@@ -33,7 +42,7 @@ const Animais = () => {
     
       <Container fluid className={styles['gridcate']}>
         <div className={styles['apresentacao']}>
-          <h1 className="text-[1.875rem] font-[700] my-2">
+          <h1 className="text-[1.875rem] font-bold my-2">
             Pets para adoção
           </h1>
         </div>
@@ -145,63 +154,74 @@ const Animais = () => {
         <div className={styles['descricaoani']}>
 
          
-          {/* CONFIGURAR RENDERIZACAO DEPOIS 
+           
           
-          {animal.map((ani) => {
-            return (
-              <figure className={styles['figures']} key={ani._id}>
+          {animal.map((ani) => ('Imagem:', ani.imagem);(
+                
+              <figure
+                key={ani._id}
+                className="flex bg-white dark:bg-[#1e1e1e] shadow-md rounded-xl overflow-hidden mb-6"
+              >
+                
                 <img
-                  className="w-50 h-auto"
+                  className="w-[27.375rem] h-40 object-cover rounded-tl-xl rounded-bl-xl"
                   src={`http://localhost:3002${ani.imagem}`}
-                  alt="imagem"
+                  alt={ani.nome}
                 />
-                <figcaption>
-                  <p>
-                    <span className="font-bold">Nome:</span> {ani.nome}
-                  </p>
-                  <p>
-                    <span className="font-bold">Idade:</span> {ani.idade}
-                  </p>
-                  <p>
-                    <span className="font-bold">Raça:</span> {ani.raca}
-                  </p>
-                  <p>
-                    <span className="font-bold">Sexo:</span> {ani.sexo}
-                  </p>
-                  <p>
-                    <span className="font-bold">Porte:</span> {ani.porte}
-                  </p>
-                  <p>
-                    <span className="font-bold">Peso:</span> {ani.peso}
-                  </p>
-                  <p>
-                    <span className="font-bold">Animal castrado?:</span>{' '}
-                    {ani.castracao ? 'Sim' : 'Não'}
-                  </p>
-                  <p>
-                    <span className="font-bold">Observações:</span>{' '}
-                    {ani.observacoes}
-                  </p>
+
+                
+                <figcaption className="px-10 py-4 flex gap-20 w-full justify-between">
+                  
+                  <div>
+                    <span className="font-semibold text-gray-700 dark:text-gray-300">Nome</span>
+                    <p className="text-gray-900 dark:text-white mb-2">{ani.nome}</p>
+
+                    <span className="font-semibold text-gray-700 dark:text-gray-300">Gênero</span>
+                    <p className="text-gray-900 dark:text-white mb-2">{ani.sexo}</p>
+
+                    <span className="font-semibold text-gray-700 dark:text-gray-300">Raça</span>
+                    <p className="text-gray-900 dark:text-white">{ani.raca}</p>
+                  </div>
+
+                  
+                  <div>
+                    <span className="font-semibold text-gray-700 dark:text-gray-300">Idade</span>
+                    <p className="text-gray-900 dark:text-white mb-2">{ani.idade}</p>
+
+                    <span className="font-semibold text-gray-700 dark:text-gray-300">Porte</span>
+                    <p className="text-gray-900 dark:text-white mb-2">{ani.porte}</p>
+
+                    <span className="font-semibold text-gray-700 dark:text-gray-300">Peso</span>
+                    <p className="text-gray-900 dark:text-white">{ani.peso}</p>
+                  </div>
+
+                  
+                  <div className="max-w-md">
+                    <span className="font-semibold text-gray-700 dark:text-gray-300">Animal castrado?</span>
+                    <p className="text-gray-900 dark:text-white mb-2">
+                      {ani.castracao ? 'Sim' : 'Não'}
+                    </p>
+
+                    <span className="font-semibold text-gray-700 dark:text-gray-300">Observações</span>
+                    <p className="text-gray-900 dark:text-white">{ani.observacoes}</p>
+                  </div>
                 </figcaption>
               </figure>
-
-              
-            );
-          })} */}
+            ))}
 
                
             
-              <figure className={styles['figures']}>
+              {/* <figure className={styles['figures']}>
 
        
                    <img
                   className="w-[27.375rem] h-40 rounded-tl-[0.75rem] e rounded-bl-[0.75rem]"
                   src="/images/dog-.jpg"
                   alt="imagem"
-                />
+                /> */}
  
 
-                <figcaption className='px-[7rem]  flex gap-[7rem]'>
+                {/* <figcaption className='px-[7rem]  flex gap-[7rem]'>
                  
                  <div className=''>
                   <span>Nome</span>
@@ -234,7 +254,7 @@ const Animais = () => {
                  </div>
 
                 </figcaption>
-              </figure>
+              </figure> */}
         </div>
       </Container>
       <Footer />
